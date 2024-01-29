5b4bf58dafa41e96ff003428da6ed8c1eb5103e0
paulvxx <psv51037@gmail.com>
2024-01-29 00:03:17 -0700

    example

--- src/main.cpp
+++ src/main.cpp
@@ -1,1 +1,18 @@
+#include <iostream>
 
+using namespace std;
+
+//example program to get something started
+int main(int argc, char *argv[]) {
+    int x = 12;
+    if (x < 45) {
+        int i = 0;
+        while (i < 4 && x < 50) {
+            x *= 2;
+            x++;
+        }
+    }
+    int* a = new int[13];
+    cout << "Value of x is: " << x << endl;
+    return 0;
+}
